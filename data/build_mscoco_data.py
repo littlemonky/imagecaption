8b8bfe2fc56320f69e595877b139e13e66ff77b6
littlemonky <39784544+littlemonky@users.noreply.github.com>
2018-08-10 20:52:00 +0800

    Update build_mscoco_data.py

--- data/build_mscoco_data.py
+++ data/build_mscoco_data.py
@@ -193,7 +193,7 @@ def _int64_feature(value):
 
 def _bytes_feature(value):
   """Wrapper for inserting a bytes Feature into a SequenceExample proto."""
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[str(value).encode()]))
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.encode() if type(value)==str else value]))
 
 
 def _int64_feature_list(values):
